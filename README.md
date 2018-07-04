734d63e51710ffb9d56ef59e9c87c5a6f1acb18a
dan.paris <dparis44@gmail.com>
2018-07-04 20:32:59 +0100

    Initial project commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+#Project Summary
+This project is a REST API that will expose an end-point taken an integer argument and return all the prime numbers up to, and including, that number. 
---
+++ src/main/java/danparis/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package danparis.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+    
+}
